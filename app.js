51d274f3dbdd825b20754046dcf90b4b1fd5750d
jongrover <hello@jonathangrover.com>
2020-08-05 19:04:05 -0400

    add solution

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var music, sentence;
 
 // Write your solution code below here using the variables declared above.
 
+music = ['Bon Jovi', 'Little Richard', 'Michael Jackson'];
 
+music.push('Prince');
+
+sentence = 'My favorite artists are '+music.join(', ');
 
 // Leave this portion of code alone to make testing work.
 
